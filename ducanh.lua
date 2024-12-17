ef32f2086fdfb9d81d54fb8f8f08152d2b80e23c
Ducanh75 <dinhducanh07052011@gmail.com>
2024-12-17 23:37:02 +0700

    Add files via upload

---
+++ ducanh.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "ducanh75",--Tên Script
+    logoscript = "1008078527",--ID Logo Script
+    tacgia = "đức anh nè"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
